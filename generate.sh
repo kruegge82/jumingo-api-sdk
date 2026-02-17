a400a4ed1838924e39eb6e9906a579029dae39d0
Jahn Nietzke <jahn@sendagents.com>
2026-02-17 13:09:28 +0100

    Stop tracking response.json

--- generate.sh
+++
@@ -1,6 +0,0 @@
-# https://connect.cargointernational.de/api/documentation#/
-cd /usr/local/bin
-openapi-generator-cli.sh version
-
-./openapi-generator-cli.sh generate -i /Users/jahn/PhpstormProjects/jumingo-api-sdk/1.0.4.json -g php -o /Users/jahn/PhpstormProjects/jumingo-api-sdk --enable-post-process-file --additional-properties=composerPackageName=kruegge82/jumingo,invokerPackage=kruegge82\\\jumingo
-rm -rf /Users/jahn/PhpstormProjects/jumingo-api-sdk/test
